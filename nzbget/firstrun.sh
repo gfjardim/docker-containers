39652000bec28ad74146f70ce51bab0621a58fb6
gfjardim <Guilherme@MacBook-Air-de-Guilherme.local>
2014-09-16 15:33:33 -0300

    Add SSL option

--- nzbget/firstrun.sh
+++ nzbget/firstrun.sh
@@ -30,9 +30,20 @@ else
   sed -i -e "s#\(UMask=\).*#\1000#g" /config/nzbget.conf
   sed -i -e "s#\(ScriptDir=\).*#\1/config/ppscripts#g" /config/nzbget.conf
   sed -i -e "s#\(QueueDir=\).*#\1/config/queue#g" /config/nzbget.conf
-  sed -i -e "s#\(LogFile=\).*#\1/config/log/nzbget.logs#g" /config/nzbget.conf
+  sed -i -e "s#\(LogFile=\).*#\1/config/log/nzbget.log#g" /config/nzbget.conf
+  sed -i -e "s#\(SecureControl=\).*#\1yes#g" /config/nzbget.conf
+  sed -i -e "s#\(SecurePort=\).*#\16791#g" /config/nzbget.conf
+  sed -i -e "s#\(SecureCert=\).*#\1/config/ssl/nzbget.crt#g" /config/nzbget.conf
+  sed -i -e "s#\(SecureKey=\).*#\1/config/ssl/nzbget.key#g" /config/nzbget.conf
   mkdir -p /downloads/dst
 fi
+
+if [[ ! -f /config/ssl/nzbget.key ]]; then
+  mkdir -p /config/ssl
+  openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /config/ssl/nzbget.key -out /config/ssl/nzbget.crt -subj "/O=SOHO/OU=HOME/CN=yourhome.com"
+fi
+
+
 
 # Embed the update package-info
 if [[ ! -f /usr/share/nzbget/webui/package-info.json ]]; then
