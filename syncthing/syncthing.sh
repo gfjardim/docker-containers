4d7f1075b923f8100078eed66902b7017bd8e6bb
gfjardim <Guilherme@MacBook-Air-de-Guilherme.local>
2014-09-17 15:51:41 -0300

    Update Syncthing

--- syncthing/syncthing.sh
+++ syncthing/syncthing.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 umask 000
-STGUIADDRESS="https://0.0.0.0:8080" 
 
-chown -R nobody:users /opt/syncthing
+chown -R nobody:users /opt/syncthing /config/config /config/files
 
-exec /sbin/setuser nobody /opt/syncthing/syncthing -home="/config"
+exec /sbin/setuser nobody /opt/syncthing/syncthing -home="/config" -gui-address="https://0.0.0.0:8080" 
