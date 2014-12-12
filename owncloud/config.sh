811c9eea1faf581c5397858465bc95be4d1ce6d9
gfjardim <Guilherme@MacBook-Air-de-Guilherme.local>
2014-12-12 16:24:45 -0200

    update ownCloud

--- owncloud/config.sh
+++ owncloud/config.sh
@@ -4,6 +4,7 @@
 if [[ $(cat /etc/timezone) != $TZ ]] ; then
   echo "$TZ" > /etc/timezone
   dpkg-reconfigure -f noninteractive tzdata
+  sed -i -e "s#;date.timezone.*#date.timezone = ${TZ}#g" /etc/php5/fpm/php.ini
 fi
 
 
