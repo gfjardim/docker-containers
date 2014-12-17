a7ce9cd0fd276b3b002a7720df7d6f681cd593a8
gfjardim <gfjardim@gmail.com>
2014-12-17 19:28:59 -0200

    ownCloud: Fix php-fpm not starting up

---
+++ madsonic/config.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Fix the timezone
+if [[ $(cat /etc/timezone) != $TZ ]] ; then
+  echo "$TZ" > /etc/timezone
+  dpkg-reconfigure -f noninteractive tzdata
+fi
+
--- madsonic/install.sh
+++ madsonic/install.sh
@@ -20,36 +20,32 @@ rm -rf /etc/service/sshd /etc/my_init.d/00_regen_ssh_host_keys.sh
 # Repositories
 add-apt-repository "deb http://us.archive.ubuntu.com/ubuntu/ trusty universe multiverse"
 add-apt-repository "deb http://us.archive.ubuntu.com/ubuntu/ trusty-updates universe multiverse"
+add-apt-repository ppa:webupd8team/java
+
+# Accept JAVA license
+echo "oracle-java7-installer shared/accepted-oracle-license-v1-1 select true" | /usr/bin/debconf-set-selections
+
+# Use mirrors
+sed -i -e "s#http://[^\s]*archive.ubuntu[^\s]* #mirror://mirrors.ubuntu.com/mirrors.txt #g" /etc/apt/sources.list
 
 # Install Dependencies
 apt-get update -qq
-apt-get install -qy php5-cli \
-                    php5-gd \
-                    php5-pgsql \
-                    php5-sqlite \
-                    php5-mysqlnd \
-                    php5-curl \
-                    php5-intl \
-                    php5-mcrypt \
-                    php5-ldap \
-                    php5-gmp \
-                    php5-apcu \
-                    php5-imagick \
-                    php5-fpm \
-                    smbclient \
-                    nginx \
-                    openssl
+apt-get install -qy unzip gzip oracle-java7-installer wget
 
 #########################################
 ##             INSTALLATION            ##
 #########################################
 
-cp /etc/php5/fpm/php-fpm.conf.dpkg-new /etc/php5/fpm/php-fpm.conf
+MADSONIC_LINK="http://madsonic.org/download/5.2/20141214_madsonic-5.2.5420-standalone.tar.gz"
+TRANSCODE_LINK="http://madsonic.org/download/transcode/20141017_madsonic-transcode_latest_x64.zip"
 
-#########################################
-##                 CLEANUP             ##
-#########################################
+# Install Madsonic
+mkdir -p /opt/madsonic /opt/transcode
+wget -qO - "${MADSONIC_LINK}" | tar -zx -C /opt/madsonic
+wget -qO /tmp/transcode.zip "${TRANSCODE_LINK}"
+unzip /tmp/transcode.zip -d /opt/transcode
+rm -rf /tmp/transcode.zip /opt/transcode/windows /opt/transcode/mac /opt/transcode/licenses
 
 # Clean APT install files
 apt-get clean -y
-rm -rf /var/lib/apt/lists/* /var/cache/* /var/tmp/*
+rm -rf /var/lib/apt/lists/* /var/tmp/* /var/cache/*
