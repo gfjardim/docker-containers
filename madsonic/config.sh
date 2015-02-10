97339bc244b6e496bbf76ac7b64c16c3461d27aa
cocasema <cocasema@yandex.ru>
2015-02-10 01:25:41 -0800

    FIX checking if  is set

--- madsonic/config.sh
+++ madsonic/config.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 
 # Fix the timezone
-if [[ $(cat /etc/timezone) != $TZ ]] ; then
-  echo "$TZ" > /etc/timezone
-  dpkg-reconfigure -f noninteractive tzdata
+if [[ $TZ ]]; then
+  if [[ $(cat /etc/timezone) != $TZ ]] ; then
+    echo "Updating timezone to '$TZ'"
+    echo "$TZ" > /etc/timezone
+    dpkg-reconfigure -f noninteractive tzdata
+  fi
 fi
 
 # Updating UID/GID for nobody
-if [ -z "$USERMAP_UID" ]; then
+if [[ ! $USERMAP_UID ]]; then
     echo "Using default uid of 'nobody' $(id -u nobody)"
 else
     echo "Updating 'nobody' uid to $USERMAP_UID"
     usermod -u ${USERMAP_UID} nobody
 fi
-if [ -z "$USERMAP_GID" ]; then
+if [[ ! $USERMAP_GID ]]; then
     echo "Using default gid of 'nobody' $(id -g nobody)"
 else
     echo "Updating 'nobody' gid to $USERMAP_GID"
