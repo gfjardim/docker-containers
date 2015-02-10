99151f130b46dd80c9ed3a2db236d4ff42635f32
cocasema <cocasema@yandex.ru>
2015-02-10 00:16:49 -0800

    Updating UID/GID for nobody

--- madsonic/config.sh
+++ madsonic/config.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+# Updating UID/GID for nobody
+if [ -z "$USERMAP_UID" ]; then
+    echo "Using default uid of 'nobody' $(id -u nobody)"
+else
+    echo "Updating 'nobody' uid to $USERMAP_UID"
+    usermod -u ${USERMAP_UID} nobody
+fi
+if [ -z "$USERMAP_GID" ]; then
+    echo "Using default gid of 'nobody' $(id -g nobody)"
+else
+    echo "Updating 'nobody' gid to $USERMAP_GID"
+    usermod -g ${USERMAP_GID} nobody
+fi
+
 # Fix the timezone
 if [[ $(cat /etc/timezone) != $TZ ]] ; then
   echo "$TZ" > /etc/timezone
