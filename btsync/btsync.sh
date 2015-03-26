0d541094cf09abd56b5adab5feccdd777cd6945b
gfjardim <gfjardim@gmail.com>
2015-03-26 17:31:21 -0300

    Update btsync

--- btsync/btsync.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-umask 000
-
-[[ ! -f /config/btsync.conf ]] && cp /tmp/btsync.conf /config/
-[[ ! -d /config/.sync ]] && mkdir -p /config/.sync
-
-chown -R nobody:users /opt/btsync /config
-
-exec /sbin/setuser nobody /opt/btsync/btsync --nodaemon --config "/config/btsync.conf"
--- btsync/install.sh
+++ btsync/install.sh
@@ -5,6 +5,7 @@
 #########################################
 
 # Configure user nobody to match unRAID's settings
+export DEBIAN_FRONTEND="noninteractive"
 usermod -u 99 nobody
 usermod -g 100 nobody
 usermod -d /home nobody
@@ -12,6 +13,46 @@ chown -R nobody:users /home
 
 # Disable SSH
 rm -rf /etc/service/sshd /etc/my_init.d/00_regen_ssh_host_keys.sh
+
+#########################################
+##  FILES, SERVICES AND CONFIGURATION  ##
+#########################################
+#config
+cat <<'EOT' > /etc/my_init.d/config.sh
+if [[ $(cat /etc/timezone) != $TZ ]] ; then
+  echo "$TZ" > /etc/timezone
+  dpkg-reconfigure -f noninteractive tzdata
+fi
+EOT
+
+# btsync
+mkdir -p /etc/service/btsync
+cat <<'EOT' > /etc/service/btsync/run
+#!/bin/bash
+umask 000
+[[ ! -f /config/btsync.conf ]] && cp /tmp/btsync.conf /config/
+[[ ! -d /config/.sync ]] && mkdir -p /config/.sync
+chown -R nobody:users /opt/btsync /config
+exec /sbin/setuser nobody /opt/btsync/btsync --nodaemon --config "/config/btsync.conf"
+EOT
+
+cat <<'EOT' > /tmp/btsync.conf
+{
+  "device_name": "unRAID",
+  "listening_port" : 5555, 
+  "storage_path" : "/config/.sync",
+  "use_upnp" : false,
+
+  "download_limit" : 0,
+  "upload_limit" : 0,
+  "webui" :
+  {
+    "listen" : "0.0.0.0:8888"
+  }
+}
+EOT
+
+chmod -R +x /etc/service/ /etc/my_init.d/
 
 #########################################
 ##    REPOSITORIES AND DEPENDENCIES    ##
@@ -30,7 +71,8 @@ apt-get install -qy wget
 #########################################
 
 # Install BTSync
-mkdir -p /opt/btsync && wget -nv -O - "https://download-cdn.getsyncapp.com/stable/linux-x64/BitTorrent-Sync_x64.tar.gz" |  tar -xzf - -C /opt/btsync
+mkdir -p /opt/btsync
+wget -nv -O - "https://download-cdn.getsyncapp.com/stable/linux-x64/BitTorrent-Sync_x64.tar.gz" |  tar -xzf - -C /opt/btsync
 
 #########################################
 ##                 CLEANUP             ##
