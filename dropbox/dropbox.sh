442c69be41d1955e0c2c5bcd2d661a3e5ba805a3
HurricaneHernandez <carlos@techbyte.ca>
2015-04-16 11:39:04 -0600

    Updating Dropbox continaer: add DropboxStatus: it's a ruby script that listen to Dropbox's socket in order to print messages from Dropbox that would normally pop up in the system tray appear in the console. mv dropbox start run script to be here document adjusted run script to force dropbox to start in case of in proper shutdown, removes socket files and pid file.

--- dropbox/dropbox.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-umask 000
-chown -R nobody:users /home
-
-exec /sbin/setuser nobody /home/.dropbox-dist/dropboxd
--- dropbox/install.sh
+++ dropbox/install.sh
@@ -23,7 +23,57 @@ add-apt-repository "deb http://us.archive.ubuntu.com/ubuntu/ trusty-updates univ
 
 # Install Dependencies
 apt-get update -qq
-apt-get install -qy wget
+apt-get install -qy wget ruby
+
+#########################################
+##  FILES, SERVICES AND CONFIGURATION  ##
+#########################################
+# CONFIG
+cat <<'EOT' > /etc/my_init.d/config.sh
+#!/bin/bash
+# Fix the timezone
+if [[ $(cat /etc/timezone) != $TZ ]] ; then
+  echo "$TZ" > /etc/timezone
+  dpkg-reconfigure -f noninteractive tzdata
+fi
+
+if [ -z "$STATUS" ]; then
+  echo "Continous console status not requested"
+  [ -d /etc/service/DropboxStatus ] && rm -r /etc/service/DropboxStatus
+fi
+EOT
+
+# Dropbox
+mkdir -p /etc/service/Dropbox
+cat <<'EOT' > /etc/service/Dropbox/run
+#!/bin/bash
+umask 000
+chown -R nobody:users /home
+
+#  Dropbox did not shutdown properly? Remove files.
+[ ! -e "/home/.dropbox/command_socket" ] || rm /home/.dropbox/command_socket
+[ ! -e "/home/.dropbox/iface_socket" ]   || rm /home/.dropbox/iface_socket
+[ ! -e "/home/.dropbox/unlink.db" ]      || rm /home/.dropbox/unlink.db
+[ ! -e "/home/.dropbox/dropbox.pid" ]    || rm /home/.dropbox/dropbox.pid
+
+exec /sbin/setuser nobody /home/.dropbox-dist/dropboxd
+EOT
+
+# DropboxStatus
+mkdir -p /etc/service/DropboxStatus
+cat <<'EOT' > /etc/service/DropboxStatus/run
+#!/bin/bash                                                                                   
+exec 2>&1 
+
+# Wait for Dropbox Daemon to start.
+[ -e /home/.dropbox/iface_socket ] || exit 1
+
+
+# start DropboxStatus
+exec /sbin/setuser nobody /usr/bin/dropbox_status
+EOT
+
+chmod +x /etc/service/*/run /etc/my_init.d/*
 
 #########################################
 ##             INSTALLATION            ##
@@ -31,6 +81,57 @@ apt-get install -qy wget
 
 # Install Dropbox
 wget -nv -O - "https://www.dropbox.com/download?plat=lnx.x86_64" | tar -xzf - -C /home
+
+# Install DropboxStatus ruby script
+cat << 'EOT' > /usr/bin/dropbox_status
+#!/usr/bin/ruby
+# dropbox status in console.
+# http://dl.getdropbox.com/u/76825/dropbox_status.rb
+# http://d.hatena.ne.jp/urekat/20081124/1227498262
+# http://forums.getdropbox.com/tags.php?tag=cli
+
+require "socket"
+require "pathname"
+
+iface_sock_path   = File.expand_path("/home/.dropbox/iface_socket")
+s_iface   = UNIXSocket.open(iface_sock_path)
+
+def cmd_done(lines)
+  case lines[0]
+  when "change_to_menu"
+    # "active\ttrue"
+  when "change_state"
+    # "new_state\t1"
+  when "refresh_tray_menu"
+    # "active\ttrue"
+  when "shell_touch"
+    puts "[#{Time.now.inspect}] "+lines[1].gsub(/^path\t/, "")
+  when "bubble"
+    s = "[#{Time.now.inspect}]--------"
+    puts s
+    lines[1,lines.size-2].each do |l|
+      puts "    "+l
+    end
+    puts "-" * s.size
+  else
+    puts "unknown:"+lines.inspect
+  end
+end
+
+lines = []
+loop{
+  l = s_iface.gets
+  break unless l
+  l.strip!
+  lines << l
+  if l=="done"
+    cmd_done(lines)
+    lines = []
+  end
+}
+EOT
+
+chmod +x /usr/bin/dropbox_status
 
 #########################################
 ##                 CLEANUP             ##
