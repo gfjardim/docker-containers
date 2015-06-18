3f633cd0f16a3648718f2cbb4b0c021b9686840d
gfjardim <gfjardim@gmail.com>
2015-06-18 19:03:32 -0300

    update CUPS container

--- cups/firstrun.sh
+++ cups/firstrun.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-mkdir -p /config/cups /config/spool /config/logs /config/cache /config/cups/ssl /config/cloudprint
+mkdir -p /config/cups /config/spool /config/logs /config/cache /config/cups/ssl /config/cups/ppd /config/cloudprint
 
+# Copy missing config files
 cd /etc/cups
 for f in *.conf ; do 
   if [ ! -f "/config/cups/${f}" ]; then
@@ -9,7 +10,14 @@ for f in *.conf ; do
   fi
 done
 
-if [ ! -f "/config/cloudprint/Service State" ]; then
-  /config/cloudprint
-  python /opt/generate_cloudprint_config.py
+# CloudPrint
+if [[ -n ${CLOUD_PRINT_EMAIL} ]]; then
+  # Create auth token
+  if [[ $(grep -c 'auth_token' '/config/cloudprint/Service State') -eq 0 ]]; then
+    cd /config/cloudprint
+    python /opt/generate_cloudprint_config.py
+  fi
+else
+  # Disable CloudPrint
+  chmod -x /etc/service/chrome/run
 fi
